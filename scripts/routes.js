1a76878f3c66d7d2ce7c1b7b03db971909600a26
Hugo A Guerra <guerrahugoa@gmail.com>
2016-02-28 19:12:41 -0800

    added routes.js controllers

---
+++ scripts/routes.js
@@ -0,0 +1,6 @@
+page('/', indexController.index);
+page('/grill-menu', menuController.index);
+page('/products', productsController.index);
+page('/friends' friendsController.index);
+page('/media', mediaController.index);
+page('/contact-us' contactController.index);
